50e4d56b77275875a992b81e796c60c733ccd243
Paulo Olivo <paulo.olivo@udla.edu.ec>
2024-10-27 22:11:36 -0500

    Paulo Olivo: Modificacion Aboutcontroller, Cambio de fondo de la web, migracion para About us

--- Controllers/AboutController.cs
+++ Controllers/AboutController.cs
@@ -43,6 +43,112 @@ namespace SPJ_ProyectoMVC.Controllers
             return View(about);
         }
 
+        // GET: About/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: About/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("AboutId,Description")] About about)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(about);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(about);
+        }
+
+        // GET: About/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var about = await _context.About.FindAsync(id);
+            if (about == null)
+            {
+                return NotFound();
+            }
+            return View(about);
+        }
+
+        // POST: About/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("AboutId,Description")] About about)
+        {
+            if (id != about.AboutId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(about);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AboutExists(about.AboutId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(about);
+        }
+
+        // GET: About/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var about = await _context.About
+                .FirstOrDefaultAsync(m => m.AboutId == id);
+            if (about == null)
+            {
+                return NotFound();
+            }
+
+            return View(about);
+        }
+
+        // POST: About/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var about = await _context.About.FindAsync(id);
+            if (about != null)
+            {
+                _context.About.Remove(about);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
         private bool AboutExists(int id)
         {
             return _context.About.Any(e => e.AboutId == id);
